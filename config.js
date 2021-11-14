c0555b750f0366c047c821f1a2b4f45228d7f116
Tom Hindby <tom@uppercenter.dev>
2021-11-14 23:22:26 +0000

    Added API Key to public repo kekw

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   greetingNight: 'Go to Sleep!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
+  weatherKey: 'dea2bc2b57b8f96f84039a289988f9c5',
   weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
